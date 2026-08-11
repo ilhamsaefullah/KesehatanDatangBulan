f44d92a9bdc51e5e897a809761ab10ebc976498b
ilhamsaefullah <ilhamsaefullah0123@gmail.com>
2026-08-12 06:17:19 +0700

    Update Kalender.js

--- Kalender.js
+++ Kalender.js
@@ -19,10 +19,83 @@ const namaBulan = [
   "Desember",
 ];
 
-function tampilkanBulan() {
+function tampilkanKalender() {
+  // Tampilkan nama bulan dan tahun
   document.getElementById("namaBulan").textContent =
     namaBulan[bulanSekarang] + " " + tahunSekarang;
+
+  const kalender = document.getElementById("tanggalKalender");
+
+  // Kosongkan tanggal sebelumnya
+  kalender.innerHTML = "";
+
+  // Menentukan jumlah hari dalam bulan
+  const jumlahHari = new Date(tahunSekarang, bulanSekarang + 1, 0).getDate();
+
+  // Membuat tanggal
+  for (let tanggal = 1; tanggal <= jumlahHari; tanggal++) {
+    const kotakTanggal = document.createElement("div");
+
+    kotakTanggal.textContent = tanggal;
+
+    // =========================
+    // TANGGAL MENSTRUASI
+    // =========================
+
+    if (
+      bulanSekarang === 7 &&
+      tahunSekarang === 2026 &&
+      tanggal >= 15 &&
+      tanggal <= 19
+    ) {
+      kotakTanggal.classList.add("menstruasi");
+    }
+
+    // =========================
+    // TANGGAL PREDIKSI
+    // =========================
+
+    if (
+      bulanSekarang === 8 &&
+      tahunSekarang === 2026 &&
+      tanggal >= 12 &&
+      tanggal <= 16
+    ) {
+      kotakTanggal.classList.add("prediksi");
+    }
+
+    // =========================
+    // MASA SUBUR
+    // =========================
+
+    if (
+      bulanSekarang === 7 &&
+      tahunSekarang === 2026 &&
+      tanggal >= 25 &&
+      tanggal <= 30
+    ) {
+      kotakTanggal.classList.add("subur");
+    }
+
+    // =========================
+    // HARI INI
+    // =========================
+
+    const hariIni = new Date();
+
+    if (
+      tanggal === hariIni.getDate() &&
+      bulanSekarang === hariIni.getMonth() &&
+      tahunSekarang === hariIni.getFullYear()
+    ) {
+      kotakTanggal.classList.add("hari-ini");
+    }
+
+    kalender.appendChild(kotakTanggal);
+  }
 }
+
+// Tombol bulan sebelumnya
 
 function bulanSebelumnya() {
   bulanSekarang--;
@@ -33,8 +106,10 @@ function bulanSebelumnya() {
     tahunSekarang--;
   }
 
-  tampilkanBulan();
+  tampilkanKalender();
 }
+
+// Tombol bulan berikutnya
 
 function bulanBerikutnya() {
   bulanSekarang++;
@@ -45,9 +120,9 @@ function bulanBerikutnya() {
     tahunSekarang++;
   }
 
-  tampilkanBulan();
+  tampilkanKalender();
 }
 
-// Jalankan saat halaman dibuka
+// Jalankan pertama kali
 
-tampilkanBulan();
+tampilkanKalender();
