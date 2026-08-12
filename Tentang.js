34dec83ba989d408b636c59b9063563f327073b0
ilhamsaefullah <ilhamsaefullah0123@gmail.com>
2026-08-12 18:12:45 +0700

    Create Tentang.js

---
+++ Tentang.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Tempat menaruh logic interaktif jika diperlukan di kemudian hari
+  console.log("Halaman Tentang Menstrual Care berhasil dimuat.");
+});
