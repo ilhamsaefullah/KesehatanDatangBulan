ac5e5cc6dc56efae04f5d05fe0c380723009b5c8
ilhamsaefullah <ilhamsaefullah0123@gmail.com>
2026-08-13 17:19:40 +0700

    Update Tentang.js

--- Tentang.js
+++ Tentang.js
@@ -2,3 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // Tempat menaruh logic interaktif jika diperlukan di kemudian hari
   console.log("Halaman Tentang Menstrual Care berhasil dimuat.");
 });
+
+//Button Back//
+const btnBack = document.getElementById("btnBack");
+btnBack.addEventListener("click", () => {
+  // Kembali ke halaman sebelumnya
+  window.history.back();
+});
